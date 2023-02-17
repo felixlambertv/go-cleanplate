d0705f9b70e9b0655d570df06c1d6b10506b43b0
Felix Lambert <felix@itsavirus.com>
2023-02-17 16:42:26 +0800

    Add middleware for db tx

---
+++ internal/middleware/db_transaction.go
@@ -0,0 +1,23 @@
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
+	"log"
+)
+
+func DBTransactionMiddleware(db *gorm.DB) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		txHandle := db.Begin()
+		log.Print("begin db transaction")
+
+		defer func() {
+			if r := recover(); r != nil {
+				txHandle.Rollback()
+			}
+		}()
+
+		c.Set("db_trx", txHandle)
+		c.Next()
+	}
+}
