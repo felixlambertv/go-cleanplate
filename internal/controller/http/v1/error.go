ca5f6fcdc9a18a778a381384543960b889f7459c
Felix Lambert <felix@itsavirus.com>
2023-02-19 23:04:42 +0800

    update controller, create user

--- internal/controller/http/v1/error.go
+++ internal/controller/http/v1/error.go
@@ -3,9 +3,13 @@ package v1
 import "github.com/gin-gonic/gin"
 
 type response struct {
-	Error string `json:"error"`
+	Message string `json:"message"`
+	Errors  []any  `json:"errors"`
 }
 
-func errorResponse(c *gin.Context, code int, msg string) {
-	c.AbortWithStatusJSON(code, response{msg})
+func errorResponse(c *gin.Context, code int, msg string, errors ...any) {
+	var res response
+	res.Message = msg
+	res.Errors = errors
+	c.AbortWithStatusJSON(code, res)
 }
--- internal/controller/http/v1/user.go
+++ internal/controller/http/v1/user.go
@@ -1,10 +1,12 @@
 package v1
 
 import (
+	"github.com/felixlambertv/go-cleanplate/internal/controller/request"
 	"github.com/felixlambertv/go-cleanplate/internal/service"
 	"github.com/felixlambertv/go-cleanplate/pkg/logger"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strings"
 )
 
 type userRoutes struct {
@@ -18,13 +20,28 @@ func newUserRoutes(handler *gin.RouterGroup, l logger.Interface, s service.IUser
 	h := handler.Group("users")
 	{
 		h.GET("", r.getUser)
-		h.GET("aa", r.getUser)
+		h.POST("", r.createUser)
 	}
 }
 
-type userResponse struct {
-	Name  string `json:"name"`
-	Email string `json:"email"`
+func (r *userRoutes) createUser(ctx *gin.Context) {
+	var req request.CreateUserRequest
+	err := ctx.BindJSON(&req)
+	if err != nil {
+		ctx.JSON(http.StatusOK, "request not valid")
+		return
+	}
+
+	user, err := r.s.CreateUser(req)
+	if err != nil {
+		if strings.Contains(err.Error(), "SQLSTATE 23505") {
+			errorResponse(ctx, http.StatusConflict, "Duplicate email", err.Error())
+		} else {
+			errorResponse(ctx, http.StatusInternalServerError, "Something went wrong", err, err.Error())
+		}
+		return
+	}
+	ctx.JSON(http.StatusOK, user)
 }
 
 func (r *userRoutes) getUser(ctx *gin.Context) {
