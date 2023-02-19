f502cf30b8e4da2b7419b71c5780b574cee62ef9
Felix Lambert <felix@itsavirus.com>
2023-02-19 23:04:06 +0800

    update model, using gorm

--- internal/model/user.go
+++ internal/model/user.go
@@ -1,8 +1,16 @@
 package model
 
+import (
+	"gorm.io/gorm"
+	"time"
+)
+
 type User struct {
-	ID       uint
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"-"`
+	ID        uint      `gorm:"primary_key" json:"id"`
+	Name      string    `json:"name" gorm:"not null" example:"user name"`
+	Email     string    `json:"email" gorm:"not null;unique" example:"email@email.com"`
+	Password  string    `json:"-" gorm:"not null" example:"password123"`
+	CreatedAt time.Time `json:"createdAt,omitempty" example:"2023-01-01T15:01:00+00:00"`
+	UpdatedAt time.Time `json:"updatedAt,omitempty" example:"2023-02-11T15:01:00+00:00"`
+	DeletedAt gorm.DeletedAt
 }
